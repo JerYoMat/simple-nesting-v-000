197ff8c06181d6e72ac53af7917ca2ae1e292c17
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-29 16:08:32 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -107,9 +107,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-    programmer_hash[:alan_kay][:known_for] = "GUI"
-binding.pry
-programmer_hash 
+     programmer_hash[:alan_kay].each do |key, value|
+      
+     end 
 end
 
 def adding_to_dennis
